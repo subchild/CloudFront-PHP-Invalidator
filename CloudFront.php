d91b166478c08b088618dada5b991050edf865a7
Abhishek Gupta <abhi.gupta200297@gmail.com>
2013-01-12 13:45:14 -0500

    Upated to latest cloudfront API and made a couple refactorings

--- CloudFront.php
+++ CloudFront.php
@@ -29,35 +29,34 @@ class CloudFront {
 		$this->distributionId = $distributionId;
 		$this->serviceUrl     = $serviceUrl;		
 	}
-	
-	
-	/**
-	 * Invalidates object with passed key on CloudFront
-	 * @param $key 	{String|Array} Key of object to be invalidated, or set of such keys
-	 */   
+
+
+    /**
+     * Invalidates object with passed key on CloudFront
+     * @param $keys
+     * @param bool $debug
+     * @return bool|string
+     * @internal param $key {String|Array} Key of object to be invalidated, or set of such keys Key of object to be invalidated, or set of such keys
+     */
 	function invalidate($keys, $debug=false){
-		if (!is_array($keys)){
-			$keys = array($keys);
-		}
-		$date       = gmdate("D, d M Y G:i:s T");
-		$requestUrl = $this->serviceUrl."2010-08-01/distribution/" . $this->distributionId . "/invalidation";
-		// assemble request body
-		$body  = "<InvalidationBatch>";
-		foreach($keys as $key){
-			$key   = (preg_match("/^\//", $key)) ? $key : "/" . $key;
-			$body .= "<Path>".$key."</Path>";
-		}
-		$body .= "<CallerReference>".time()."</CallerReference>";
-		$body .= "</InvalidationBatch>";
-		// make and send request		
-		$req = & new HTTP_Request2($requestUrl, HTTP_Request2::METHOD_POST);
-		$req->setHeader("Date", $date);
-		$req->setHeader("Authorization", $this->makeKey($date));
-		$req->setHeader("Content-Type", "text/xml");
-		$req->setBody($body);
+        if (!is_array($keys)) {
+            $keys = array($keys);
+        }
+
+        $requestUrl = $this->serviceUrl . "2012-07-01/distribution/" . $this->distributionId . "/invalidation";
+        // assemble request body
+        $body = $this->makeRequestBody($keys);
+
+        // make and send request
+        $req = new HTTP_Request2($requestUrl, HTTP_Request2::METHOD_POST, array('ssl_verify_peer' => false));
+        $this->setRequestHeaders($req);
+        $req->setBody($body);
+
 		$response           = $req->send();
 		$this->responseCode = $response->getStatus();
-		if ($debug==true){
+
+
+        if ($debug==true){
 			$er = array();
 			array_push($er, "CloudFront: Invalidating Object: $key");
 			array_push($er, $requestUrl);
@@ -72,35 +71,55 @@ class CloudFront {
 			return ($this->responseCode === 201);
 		}
 	}
-	
-	
-	/**
-	 * Returns header string containing encoded authentication key
-	 * @param 	$date 		{Date}
-	 * @return 	{String}
-	 */
-	function makeKey($date){
-		return "AWS " . $this->accessKeyId . ":" . base64_encode($this->hmacSha1($this->secretKey, $date));
-	}
-	
-	
-	/**
-	 * Returns HMAC string
-	 * @param 	$key 		{String}
-	 * @param 	$date		{Date}
-	 * @return 	{String}
-	 */	
-	function hmacSha1($key, $date){
-		$blocksize = 64;
-		$hashfunc  = 'sha1';
-		if (strlen($key)>$blocksize){
-			$key = pack('H*', $hashfunc($key));
-		}
-		$key  = str_pad($key,$blocksize,chr(0x00));
-		$ipad = str_repeat(chr(0x36),$blocksize);
-		$opad = str_repeat(chr(0x5c),$blocksize);
-		$hmac = pack('H*', $hashfunc( ($key^$opad).pack('H*',$hashfunc(($key^$ipad).$date)) ));
-		return $hmac;
-	}
+
+
+    /**
+     * Sets the common headers required by CloudFront API
+     * @param HTTP_Request2 $req
+     */
+    private function setRequestHeaders(HTTP_Request2 $req)
+    {
+        $date = gmdate("D, d M Y G:i:s T");
+        $req->setHeader("Host", 'cloudfront.amazonaws.com');
+        $req->setHeader("Date", $date);
+        $req->setHeader("Authorization", $this->generateAuthKey($date));
+        $req->setHeader("Content-Type", "text/xml");
+    }
+
+    /**
+     * Makes the request body as expected by CloudFront API
+     * @param $objects objects to Invalidate
+     * @return string
+     */
+    private function makeRequestBody($objects)
+    {
+        $body = '<?xml version="1.0" encoding="UTF-8"?>';
+        $body .= '<InvalidationBatch xmlns="http://cloudfront.amazonaws.com/doc/2012-07-01/">';
+        $body .= '<Paths>';
+        $body .= '<Quantity>' . count($objects) . '</Quantity>';
+        $body .= '<Items>';
+        foreach ($objects as $object) {
+            $object = (preg_match("/^\//", $object)) ? $object : "/" . $object;
+            $body .= "<Path>" . $object . "</Path>";
+        }
+        $body .= '</Items>';
+        $body .= '</Paths>';
+        $body .= "<CallerReference>" . time() . "</CallerReference>";
+        $body .= "</InvalidationBatch>";
+        return $body;
+    }
+
+
+    /**
+     * Returns header string containing encoded authentication key
+     * @param $date
+     * @return string
+     */
+    private function generateAuthKey($date)
+    {
+        $signature = base64_encode(hash_hmac('sha1', $date, $this->secretKey, true));
+        return "AWS " . $this->accessKeyId . ":" . $signature;
+    }
+
 }
 ?>	

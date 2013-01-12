ab57b51aaa69a341a5a93bdd616e286e2d83883a
Abhishek Gupta <abhi.gupta200297@gmail.com>
2013-01-12 14:25:54 -0500

    Changed how error handling works

--- example.php
+++ example.php
@@ -1,27 +1,32 @@
 <?
-	require 'CloudFront.php';
-	
-	// your AWS/CloudFront keys go here
-	$keyId          = ""; 
-	$secretKey      = "";
-	$distributionId = "";
-	
-	$key = ""; // String representing the existing CloudFront object to invalidate
-	
-	$cf  = new CloudFront($keyId, $secretKey, $distributionId);
+require 'CloudFront.php';
+
+// your AWS/CloudFront keys go here
+$keyId = "";
+$secretKey = "";
+$distributionId = "";
+
+$key = ""; // String representing the existing CloudFront object to invalidate
+
+$cf = new CloudFront($keyId, $secretKey, $distributionId);
+$cf->invalidate($key);
+
 ?>
 <html>
 <head>
-<style> textarea {width:100%; height:600px; font:12px/16px consolas;} </style>
+    <style> textarea {
+        width: 100%;
+        height: 600px;
+        font: 12px/16px consolas;
+    } </style>
 </head>
 <body>
-	Key: <?=$key?><br/>
-	<hr/>
-	CF call:<br/>
-	<?/* 
-	 	Passing "true" to enable debugging for the purpose of this example. 
-		This will render the XML response.
-	*/?>
-	<textarea><?=$cf->invalidate($key, true)?></textarea>
+Key: <?=$key?><br/>
+<hr/>
+CF call:<br/>
+<!--Passing "true" to enable debugging for the purpose of this example.
+This will render the XML response.-->
+
+<textarea><?=$cf->getResponseMessage()?></textarea>
 </body>
 </html>
